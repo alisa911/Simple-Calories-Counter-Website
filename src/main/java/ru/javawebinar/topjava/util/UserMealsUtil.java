d848c281010c9ccb39c24dfcbf22f237cd405afd
alisa911 <alisa.91150@gmail.com>
2019-06-07 15:02:26 +0300

    HW0 change getFilteredWithExceededStream

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -19,9 +19,8 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+        getFilteredWithExceededStream(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+
     }
 
     public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -56,15 +55,12 @@ public class UserMealsUtil {
                                 (UserMeal::getCalories)
                         ));
 
-        List<UserMealWithExceed> newList = mealList
+        return mealList
                 .stream()
                 .filter(userMeal -> userMeal.getDateTime().toLocalTime().isAfter(startTime) && userMeal.getDateTime().toLocalTime().isBefore(endTime))
-                .map(meal -> {
-                    if (map.get(meal.getDateTime().getDayOfMonth()) > caloriesPerDay)
-                        return new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), true);
-                    else
-                        return new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), false);
-                }).collect(Collectors.toList());
-        return newList;
+                .map(meal ->
+                        new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), map.get(meal.getDateTime().getDayOfMonth()) > caloriesPerDay))
+                .collect(Collectors.toList());
+
     }
 }
