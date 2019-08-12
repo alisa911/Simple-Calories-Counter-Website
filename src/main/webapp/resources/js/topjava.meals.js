99d335405ba558fbd892b834e02f5cf626f3308a
alisa911 <alisa.91150@gmail.com>
2019-08-12 19:00:10 +0300

    add_ajax_for_meals

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -1,3 +1,9 @@
+const mealsAjaxUrl = 'ajax/profile/meals/';
+const startDate = $('#startDate');
+const endDate = $('#endDate');
+const startTime = $('#startTime');
+const endTime = $('#endTime');
+
 function updateFilteredTable() {
     $.ajax({
         type: "GET",
@@ -11,39 +17,105 @@ function clearFilter() {
     $.get("ajax/profile/meals/", updateTableByData);
 }
 
+function initDateTimePickers() {
+    $.datetimepicker.setLocale('ru');
+    startDate.datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        onShow: function() {
+            if (endDate.val())
+                this.setOptions({ maxDate: endDate.val() });
+        }
+    });
+    endDate.datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        onShow: function() {
+            if (startDate.val())
+                this.setOptions({ minDate: startDate.val() });
+        }
+    });
+    startTime.datetimepicker({
+        datepicker: false,
+        format: 'H:i',
+        onShow: function() {
+            if (endTime.val())
+                this.setOptions({ maxTime: endTime.val() });
+        }
+    });
+    endTime.datetimepicker({
+        datepicker: false,
+        format: 'H:i',
+        onShow: function() {
+            if (startTime.val())
+                this.setOptions({ minTime: startTime.val() });
+        }
+    });
+    $('#dateTime').datetimepicker({
+        format: 'Y-m-d H:i'
+    });
+}
+
+function saveMeal() {
+    let data = '';
+    document.querySelectorAll('#editRow input').forEach(element => {
+        let value = element.name === 'dateTime' ? element.value.replace(' ', 'T') : element.value;
+    if (element.value !== '')
+        data += `${element.name}=${value}&`;
+});
+    save(data.substring(0, data.length - 1));
+}
+
 $(function () {
     makeEditable({
         ajaxUrl: mealsAjaxUrl,
         datatableApi: $("#datatable").DataTable({
+            "ajax": {
+                "url": mealsAjaxUrl,
+                "dataSrc": ""
+            },
             "paging": false,
             "info": true,
             "columns": [
                 {
-                    "data": "dateTime"
-                },
-                {
-                    "data": "description"
-                },
-                {
-                    "data": "calories"
-                },
-                {
-                    "defaultContent": "",
-                    "orderable": false,
-                    "render": renderEditBtn
-                },
-                {
-                    "defaultContent": "Delete",
-                    "orderable": false
-                }
-            ],
-            "order": [
-                [
-                    0,
-                    "desc"
-                ]
-            ]
-        }),
-        updateTable: updateFilteredTable
-    });
+                    "data": "dateTime",
+                    "render": (data, type, row) => {
+                    if (type === 'display')
+        return data.substring(0, 16).replace('T', ' ');
+    return data;
+}
+},
+    {
+        "data": "description"
+    },
+    {
+        "data": "calories"
+    },
+    {
+        "defaultContent": "",
+        "orderable": false,
+        "render": renderEditBtn
+    },
+    {
+        "defaultContent": "Delete",
+        "orderable": false,
+        "render": renderDeleteBtn
+    }
+],
+    "createdRow": function ( row, data, index ) {
+        $(row).attr("data-mealExcess", data.excess);
+        // вот это я уже скопировал из демо,
+        // у самого не получилось сделать именно 'data-mealExcess'
+    },
+    "order": [
+        [
+            0,
+            "desc"
+        ]
+    ]
+}),
+    updateTable: updateFilteredTable
+});
+
+    initDateTimePickers();
 });
